211d3733142732ff12dff06af054123e9e8e3fda
김도하 <dorong0205@gmail.com>
2024-05-07 16:09:18 +0900

    :sparkles: Fix: token, service 정보 추가 및 서비스 흐름 파악용 traceProcesor 생성

--- omegi-opentelemetry-javaagent/src/main/java/org/omegi/omegiextension/SampleOmegiTraceSpanExporter.java
+++ omegi-opentelemetry-javaagent/src/main/java/org/omegi/omegiextension/SampleOmegiTraceSpanExporter.java
@@ -3,26 +3,27 @@ package org.omegi.omegiextension;
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 import io.opentelemetry.api.common.AttributeKey;
+import io.opentelemetry.api.trace.Span;
 import io.opentelemetry.api.trace.StatusCode;
 import io.opentelemetry.sdk.common.CompletableResultCode;
 import io.opentelemetry.sdk.common.InstrumentationScopeInfo;
 import io.opentelemetry.sdk.trace.data.EventData;
 import io.opentelemetry.sdk.trace.data.SpanData;
 import io.opentelemetry.sdk.trace.export.SpanExporter;
-
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 import java.nio.charset.StandardCharsets;
 import java.time.Instant;
 import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Properties;
 import java.util.concurrent.atomic.AtomicBoolean;
-import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
@@ -30,14 +31,14 @@ import org.apache.kafka.common.serialization.ByteArraySerializer;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.omegi.omegiextension.util.OmegiUtil;
 
-public class OmegiTraceSpanExporter implements SpanExporter {
+public class SampleOmegiTraceSpanExporter implements SpanExporter {
 
-	private static final Logger logger = Logger.getLogger(OmegiTraceSpanExporter.class.getName());
+	private static final Logger logger = Logger.getLogger(SampleOmegiTraceSpanExporter.class.getName());
 	private final AtomicBoolean isShutdown = new AtomicBoolean();
 	private final Gson gson = new Gson();
 	private final KafkaProducer<String, byte[]> kafkaProducer;
 
-	public OmegiTraceSpanExporter() {
+	public SampleOmegiTraceSpanExporter() {
 		this.kafkaProducer = createKafkaProducer();
 	}
 
@@ -50,8 +51,8 @@ public class OmegiTraceSpanExporter implements SpanExporter {
 		return new KafkaProducer<>(properties);
 	}
 
-	public static OmegiTraceSpanExporter create() {
-		return new OmegiTraceSpanExporter();
+	public static SampleOmegiTraceSpanExporter create() {
+		return new SampleOmegiTraceSpanExporter();
 	}
 
 	@Override
@@ -60,64 +61,29 @@ public class OmegiTraceSpanExporter implements SpanExporter {
 			return CompletableResultCode.ofFailure();
 		}
 
-        /*
-        에러 데이터면 error info 생성
-        에러가 아니면 종료
-         */
 		JsonObject outerJson = new JsonObject();
-		String traceEnterTime = null;
-		String traceExitTime = null;
-
 		SpanData firstSpan = spans.stream().findFirst().orElse(null);
 
 		if (firstSpan != null) {
 			StatusCode statusCode = firstSpan.getStatus().getStatusCode();
-			if (statusCode != StatusCode.ERROR) {
+			if (statusCode == StatusCode.ERROR) {
 				return CompletableResultCode.ofSuccess();
-			} else {
-				InstrumentationScopeInfo instrumentationScopeInfo = firstSpan.getInstrumentationScopeInfo();
-
-				String traceId = firstSpan.getTraceId();
-				outerJson.addProperty("tracer",
-					instrumentationScopeInfo.getName() + ":" + (instrumentationScopeInfo.getVersion() == null ? ""
-						: instrumentationScopeInfo.getVersion()));
-				outerJson.addProperty("traceId", traceId);
-				outerJson.addProperty("token", OmegiUtil.getToken());
-				outerJson.addProperty("service-name", OmegiUtil.getServiceName());
-
-				JsonObject exceptionJson = new JsonObject();
-
-				if (firstSpan.getEvents() != null && !firstSpan.getEvents().isEmpty()) {
-					EventData eventData = firstSpan.getEvents().get(0);
-					String stacktrace = eventData.getAttributes().get(AttributeKey.stringKey("exception.stacktrace"));
-					exceptionJson.addProperty("exception.type",
-						eventData.getAttributes().get(AttributeKey.stringKey("exception.type")));
-					exceptionJson.addProperty("exception.message",
-						eventData.getAttributes().get(AttributeKey.stringKey("exception.message")));
-
-					JsonObject stackFlowData = new JsonObject();
-					List<String> stacktraceList = OmegiUtil.analyzeErrorFlow(stacktrace);
-
-					int flowNumber = 1;
-					for (String traced : stacktraceList) {
-						stackFlowData.addProperty("step." + flowNumber++, traced);
-					}
-
-					exceptionJson.add("exception.flow", stackFlowData);
-					exceptionJson.addProperty("exception.stacktrace", stacktrace);
-				}
-
-				outerJson.add("error", exceptionJson);
 			}
 		}
 
-        /*
-        상세 span 정보
-         */
+		InstrumentationScopeInfo instrumentationScopeInfo = firstSpan.getInstrumentationScopeInfo();
+		String traceId = firstSpan.getTraceId();
+		outerJson.addProperty("tracer",
+			instrumentationScopeInfo.getName() + ":" + (instrumentationScopeInfo.getVersion() == null ? ""
+				: instrumentationScopeInfo.getVersion()));
+		outerJson.addProperty("traceId", traceId);
+		outerJson.addProperty("token", OmegiUtil.getToken());
+		outerJson.addProperty("service-name", OmegiUtil.getServiceName());
 
-		int spanNumber = 1;
-		for (SpanData span : spans) {
-			JsonObject jsonData = new JsonObject();
+		JsonObject jsonData = new JsonObject();
+		SpanData span = spans.stream().reduce((first, second) -> second).orElse(null);
+
+		if (span != null) {
 			jsonData.addProperty("name", span.getName());
 			jsonData.addProperty("spanId", span.getSpanId());
 			jsonData.addProperty("parentSpanId", span.getParentSpanId());
@@ -126,21 +92,10 @@ public class OmegiTraceSpanExporter implements SpanExporter {
 			jsonData.addProperty("span exit-time", OmegiUtil.getFormattedTime(span.getEndEpochNanos()));
 			jsonData.add("attributes", gson.toJsonTree(span.getAttributes()));
 
-			if (spans.size() == spanNumber) {
-				traceEnterTime = OmegiUtil.getFormattedTime(span.getStartEpochNanos());
-				traceExitTime = OmegiUtil.getFormattedTime(span.getEndEpochNanos());
-			}
-
-			outerJson.add("detailed-span" + spanNumber++, jsonData);
+			outerJson.add("spans", jsonData);
 		}
 
-		outerJson.addProperty("trace enter-time", traceEnterTime);
-		outerJson.addProperty("trace exit-time", traceExitTime);
-
-        /*
-        카프카 전송
-         */
-		ProducerRecord<String, byte[]> record = new ProducerRecord<>("test",
+		ProducerRecord<String, byte[]> record = new ProducerRecord<>("flow",
 			outerJson.toString().getBytes(StandardCharsets.UTF_8));
 		kafkaProducer.send(record);
 		return CompletableResultCode.ofSuccess();
