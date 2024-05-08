93b3acac08bdfd326194e240af2b44428977969e
Ahyoung Kim <130175561+AhyngK@users.noreply.github.com>
2024-05-09 01:26:53 +0900

    :sparkles: Feat-collect: jwt 구현

--- collect/auth/JwtService.py
+++ collect/auth/JwtService.py
@@ -1,4 +1,10 @@
+import base64
+import hashlib
+import hmac
+import json
 import os
+import time
+
 import jwt
 from dotenv import load_dotenv
 
@@ -6,14 +12,71 @@ load_dotenv()
 secret_key = os.getenv('JWT_SECRET')
 
 
-def decode_token(token: str):
+def decode_token(token: str) -> bool:
     try:
-        # json decode는 자동으로 현재 시간, exp date 비교해 만료 여부 확인해줌
-        decoded_token = jwt.decode(token, secret_key, algorithms=["HS256"])
-        # TODO 토큰에서 정보 가져오기
-        # TODO 정확한 토큰인지 확인하기 (PROJECT, SERVICE)
+        decoded_secret_key = base64.b64decode(secret_key)
+        hmac_key = hmac.new(decoded_secret_key, digestmod=hashlib.sha256).digest()
+
+        decoded_token = jwt.decode(token, hmac_key, algorithms=["HS256"])
+        service_id = decoded_token.get("serviceId")
+
+        if service_id is None:
+            print("Invalid token: Missing required claims")
+            return False
+
+        print("Token is valid")
+        print("Payload:")
         print(decoded_token)
+        return True
     except jwt.ExpiredSignatureError:
         print("Token has expired")
+        return False
     except jwt.InvalidTokenError:
+        print("Token has invalid secret")
+        return False
+
+
+def get_payload_from_token(token: str) -> tuple:
+    try:
+        parts = token.split(".")
+        if len(parts) != 3:
+            raise ValueError("Invalid token format")
+
+        payload_part = parts[1]
+        payload_decoded = base64.urlsafe_b64decode(payload_part + "==")
+        payload = json.loads(payload_decoded)
+
+        service_id = payload.get("serviceId")
+        project_id = payload.get("projectId")
+
+        if service_id is None or project_id is None:
+            raise ValueError("Missing required claims")
+
+        return project_id,service_id
+    except (ValueError, json.JSONDecodeError):
         print("Invalid token")
+        return None, None
+
+
+def create_jwt_for_service(service_id, expiration_ms):
+    now_millis = int(time.time() * 1000)
+    now_seconds = now_millis // 1000
+    exp_seconds = (now_millis + expiration_ms) // 1000
+
+    payload = {
+        "sub": "ServiceToken",
+        "serviceId": service_id,
+        "iat": now_seconds,
+        "exp": exp_seconds
+    }
+
+    decoded_secret_key = base64.b64decode(secret_key)
+    hmac_key = hashlib.sha256(decoded_secret_key).digest()
+
+    token = jwt.encode(payload, hmac_key, algorithm="HS256")
+    return token
+
+
+# jwt_token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJTZXJ2aWNlVG9rZW4iLCJwcm9qZWN0SWQiOjgsInNlcnZpY2VJZCI6MiwiaWF0IjoxNzE1MTY2NzM0LCJleHAiOjIwMzA1MjY3MzR9.oKJenUKkyTR47cMZnpmY6OBO5MQGpMSU3r1g0QhEeZU"
+# decode_token(jwt_token)
+# print(create_jwt_for_service(8,2))
