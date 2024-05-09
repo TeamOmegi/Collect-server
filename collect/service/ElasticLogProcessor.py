b0fdf4c0cea332dff362ff089244987e133b623a
Ahyoung Kim <130175561+AhyngK@users.noreply.github.com>
2024-05-09 17:09:31 +0900

    :sparkles: Feat-collect: redis queue 작성 중

--- collect/service/ElasticLogProcessor.py
+++ collect/service/ElasticLogProcessor.py
@@ -2,6 +2,12 @@
 
 # redis에서 traceId poll
 
-# elasticsearch에서 검색
 
-# 검색 결과 이어보기
+# 검색하면서 이어보기
+    # trace_id가 해당하면서 error가 비어있지 않은 trace 검색
+    # 거기의 parent id를 찾아서 다시 검색
+    # parent_id가 0000인게 있을 때까지
+        # 다 있을 경우 -> mongo
+        # 다 없을 경우
+        # -> 횟수 남으면 다시 redis에
+        # -> 아니면 버려
