4d8c839ca752c9c779e167284ab497cd5b7a3c07
Ahyoung Kim <130175561+AhyngK@users.noreply.github.com>
2024-05-09 10:59:45 +0900

    :wrench: Chore-collect: jwt 수정

--- collect/auth/JwtService.py
+++ collect/auth/JwtService.py
@@ -1,61 +1,48 @@
 import base64
 import hashlib
-import hmac
-import json
 import os
 import time
 
-import jwt
 from dotenv import load_dotenv
+from jose import jwt, JWTError
 
 load_dotenv()
 secret_key = os.getenv('JWT_SECRET')
 
 
-def decode_token(token: str) -> bool:
+def decode_token(token: str) -> tuple:
     try:
-        decoded_secret_key = base64.b64decode(secret_key)
-        hmac_key = hmac.new(decoded_secret_key, digestmod=hashlib.sha256).digest()
+        secret_key_bytes = secret_key.encode('utf-8')
+        decoded = base64.b64decode(secret_key_bytes)
+        key = hashlib.sha256(decoded).digest()
 
-        decoded_token = jwt.decode(token, hmac_key, algorithms=["HS256"])
+        decoded_token = jwt.decode(token, key=decoded, algorithms=["HS256"], options={"verify_signature": True, "verify_aud": False, "verify_iat": False, "verify_exp": False, "verify_nbf": False, "verify_iss": False, "verify_sub": False, "require": []})
+        project_id = decoded_token.get("projectId")
         service_id = decoded_token.get("serviceId")
 
-        if service_id is None:
+        if service_id or project_id is None:
             print("Invalid token: Missing required claims")
-            return False
+            return None
 
-        print("Token is valid")
-        print("Payload:")
-        print(decoded_token)
-        return True
+        return project_id, service_id
     except jwt.ExpiredSignatureError:
         print("Token has expired")
-        return False
-    except jwt.InvalidTokenError:
-        print("Token has invalid secret")
-        return False
+        return None
+    except JWTError:
+        print("Token has invalid signature or is malformed")
+        return None
+    except jwt.InvalidAlgorithmError:
+        print("Token has an invalid signing algorithm")
+        return None
+    except Exception as e:
+        print(f"An error occurred: {str(e)}")
+        return None
 
 
-def get_payload_from_token(token: str) -> tuple:
-    try:
-        parts = token.split(".")
-        if len(parts) != 3:
-            raise ValueError("Invalid token format")
-
-        payload_part = parts[1]
-        payload_decoded = base64.urlsafe_b64decode(payload_part + "==")
-        payload = json.loads(payload_decoded)
-
-        service_id = payload.get("serviceId")
-        project_id = payload.get("projectId")
-
-        if service_id is None or project_id is None:
-            raise ValueError("Missing required claims")
-
-        return project_id,service_id
-    except (ValueError, json.JSONDecodeError):
-        print("Invalid token")
-        return None, None
+def get_payload_from_token(message) -> tuple:
+    project_id = 8
+    service_id = 2
+    return project_id, service_id
 
 
 def create_jwt_for_service(service_id, expiration_ms):
@@ -70,13 +57,13 @@ def create_jwt_for_service(service_id, expiration_ms):
         "exp": exp_seconds
     }
 
-    decoded_secret_key = base64.b64decode(secret_key)
-    hmac_key = hashlib.sha256(decoded_secret_key).digest()
-
-    token = jwt.encode(payload, hmac_key, algorithm="HS256")
+    token = jwt.encode(payload, secret_key, algorithm="HS256")
     return token
 
 
-# jwt_token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJTZXJ2aWNlVG9rZW4iLCJwcm9qZWN0SWQiOjgsInNlcnZpY2VJZCI6MiwiaWF0IjoxNzE1MTY2NzM0LCJleHAiOjIwMzA1MjY3MzR9.oKJenUKkyTR47cMZnpmY6OBO5MQGpMSU3r1g0QhEeZU"
-# decode_token(jwt_token)
-# print(create_jwt_for_service(8,2))
+token='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJTZXJ2aWNlVG9rZW4iLCJwcm9qZWN0SWQiOjgsInNlcnZpY2VJZCI6MiwiaWF0IjoxNzE1MTY2NzM0LCJleHAiOjIwMzA1MjY3MzR9.BN2FctT1WJUyJZoFW_IrQtAkVB1wqc8vsi4MtBjlaww'
+print(decode_token(token))
+
+origin_token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJTZXJ2aWNlVG9rZW4iLCJwcm9qZWN0SWQiOjgsInNlcnZpY2VJZCI6MiwiaWF0IjoxNzE1MTY2NzM0LCJleHAiOjIwMzA1MjY3MzR9.oKJenUKkyTR47cMZnpmY6OBO5MQGpMSU3r1g0QhEeZU'
+print(decode_token(origin_token))
+
